c7533300afd288a14e8086a75f42d7e6d98b4d02
josip <jmucak@outlook.com>
2020-07-22 20:03:10 +0200

    inital filder structure

---
+++ wp-plugin-name.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              http://example.com
+ * @since             1.0.0
+ * @package           WP Optimization Plugin
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WP Optimization Plugin
+ * Plugin URI:        https://github.com/jmucak/wp-optimization-plugin
+ * Description:       Optimize and speed up WP site.
+ * Version:           1.0.0
+ * Author:            Josip Mucak
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       plugin-name
+ * Domain Path:       /languages
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
